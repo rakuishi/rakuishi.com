17a9d210e75fc955d5f63fbb29e8015ac59761ef
rakuishi <rakuishi@gmail.com>
2015-01-18 19:10:00 +0900

    Setup gulp

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var plumber = require('gulp-plumber');
+
+// $gulp sass
+gulp.task('sass', function() {
+		gulp.src('sass/**/*.scss')
+			.pipe(plumber())
+			.pipe(sass({
+				style: 'compressed',
+				compass : true
+			}))
+			.pipe(gulp.dest('static/css/'));
+});
+
+// $ gulp watch
+gulp.task('watch', function() {
+	gulp.watch('sass/**/*.scss', ['sass']);
+});
