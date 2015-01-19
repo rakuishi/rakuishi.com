92d97f269c756da9ec7857074b6fd69bb11fccbb
rakuishi <rakuishi@gmail.com>
2015-01-20 07:00:53 +0900

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,18 @@ var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var plumber = require('gulp-plumber');
 
-// $gulp sass
+// $ gulp sass
 gulp.task('sass', function() {
-		gulp.src('sass/**/*.scss')
-			.pipe(plumber())
-			.pipe(sass({
-				style: 'compressed',
-				compass : true
-			}))
-			.pipe(gulp.dest('static/css/'));
+    gulp.src('sass/**/*.scss')
+      .pipe(plumber())
+      .pipe(sass({
+        style: 'compressed',
+        compass : true
+      }))
+      .pipe(gulp.dest('static/css/'));
 });
 
 // $ gulp watch
 gulp.task('watch', function() {
-	gulp.watch('sass/**/*.scss', ['sass']);
+  gulp.watch('sass/**/*.scss', ['sass']);
 });
