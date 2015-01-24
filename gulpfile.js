276acd5d122cf90caf76a6d4b6a742f6f5f52538
rakuishi <rakuishi@gmail.com>
2015-01-24 13:10:27 +0900

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var plumber = require('gulp-plumber');
+var critical = require('critical');
 
 // $ gulp sass
 gulp.task('sass', function() {
@@ -16,4 +17,15 @@ gulp.task('sass', function() {
 // $ gulp watch
 gulp.task('watch', function() {
   gulp.watch('sass/**/*.scss', ['sass']);
+});
+
+gulp.task('critical', function () {
+  critical.generateInline({
+      base: 'public/',
+      src: 'index.html',
+      htmlTarget: 'index.html',
+      width: 320,
+      height: 480,
+      minify: true
+    });
 });
--- sh/publish.sh
+++ sh/publish.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 hugo
 find ~/Dropbox/Private/rakuishi.com/public/ -name ".DS_Store" | xargs rm
+gulp critical
 rsync -auv --delete ~/Dropbox/Private/rakuishi.com/public/ rakuishi@rakuishi.sakura.ne.jp:/home/rakuishi/www/
