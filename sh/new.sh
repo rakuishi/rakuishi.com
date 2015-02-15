edfe3f5fc3a8b6803af5f3133d5da44b0c60a799
rakuishi <rakuishi@gmail.com>
2015-02-15 10:44:20 +0900

    Add new.sh

---
+++ sh/new.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# sh/new.sh slug
+
+DIR=$(cd $(dirname ${0})/.. && pwd)
+SLUG=${1}
+FILENAME=$(date '+%F')-${SLUG}.md
+cd ${DIR}
+hugo new archives/${FILENAME}
+sed "s/slug = \"\"/slug = \"${SLUG}\"/" content/archives/${FILENAME} > content/archives/${FILENAME}.new
+mv content/archives/${FILENAME}.new content/archives/${FILENAME}
+open content/archives/${FILENAME}
