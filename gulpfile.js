f020f035be33495349610885c2ac113d3096b431
rakuishi <rakuishi@gmail.com>
2015-10-13 22:42:04 +0900

    Remove browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 var plumber = require('gulp-plumber');
-var browserSync = require('browser-sync');
 
 gulp.task('sass', function() {
     gulp.src('sass/**/*.scss')
@@ -13,19 +12,6 @@ gulp.task('sass', function() {
       .pipe(gulp.dest('static/assets/css/'));
 });
 
-gulp.task('browser-sync', function() {
-  browserSync({
-    server: {
-      baseDir: "./public/"
-    }
-  });
-});
-
-gulp.task('bs-reload', function () {
-  browserSync.reload();
-});
-
-gulp.task('default', ['browser-sync'], function() {
+gulp.task('default', function() {
   gulp.watch('sass/**/*.scss', ['sass']);
-  gulp.watch(['./*.html', './css/style.css'], ['bs-reload']);
 });
