f888fd8419393453795f25a74a07c1d45be1b23f
rakuishi <rakuishi@gmail.com>
2020-12-15 20:51:07 +0900

    Use `enableBackground` instead of `enable-background`

--- components/footer.js
+++ components/footer.js
@@ -81,7 +81,7 @@ export default function Footer() {
             })}
           >
             <svg
-              enable-background="new 0 0 24 24"
+              enableBackground="new 0 0 24 24"
               height="24"
               viewBox="0 0 24 24"
               width="24"
