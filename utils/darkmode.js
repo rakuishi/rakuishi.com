791144a4c6a637da8b6dbde6b3c5a21f9392ad97
rakuishi <rakuishi@gmail.com>
2022-08-13 17:21:48 +0900

    Improve on darkmode

--- utils/darkmode.js
+++ utils/darkmode.js
@@ -1,9 +1,22 @@
 /**
- * Call this methods with Effect Hook
+ * Call these methods with Effect Hook
  */
+
+export function isDarkmode() {
+  let scheme = "light";
+
+  if (localStorage.getItem("prefers-color-scheme")) {
+    scheme = localStorage.getItem("prefers-color-scheme");
+  } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    scheme = "dark";
+  }
+
+  return scheme === "dark";
+}
+
 export function toggleDarkmode() {
-  const isDarkmode = localStorage.getItem("prefers-color-scheme") === "dark";
-  const scheme = isDarkmode ? "light" : "dark";
+  const isDark = isDarkmode();
+  const scheme = isDark ? "light" : "dark";
   localStorage.setItem("prefers-color-scheme", scheme);
   document.documentElement.setAttribute("data-prefers-color-scheme", scheme);
 }
