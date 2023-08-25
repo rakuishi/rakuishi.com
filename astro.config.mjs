838d56d863b12f37c0d9554f2f9af8763f7ec151
rakuishi <rakuishi@gmail.com>
2023-08-26 06:27:12 +0900

    Use css-variables theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
-      theme: "nord",
+      theme: "css-variables",
     },
     remarkPlugins: [
       gfm, // tables
